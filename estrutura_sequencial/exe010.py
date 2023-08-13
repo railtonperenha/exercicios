1e882c13d4c9ff4711ffceedac436d497385c1d9
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-08-13 08:38:59 -0300

    Update exe010.py

--- estrutura_sequencial/exe010.py
+++ estrutura_sequencial/exe010.py
@@ -1,3 +1,6 @@
+# Faça um Programa que peça a temperatura em graus Celsius,
+# transforme e mostre em graus Fahrenheit
+
 celsius = input('Graus em Celsius: ')
 celsius = int(celsius)
 fahrenheit = (celsius * 1.8) + 32
