331d865847990a0728d51b01f6cc8c7a98474b7d
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-08-13 08:37:39 -0300

    Update exe013.py

--- estrutura_sequencial/exe013.py
+++ estrutura_sequencial/exe013.py
@@ -1,3 +1,7 @@
+# Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+# Para homens: (72.7*h) - 58
+# Para mulheres: (62.1*h) - 44.7
+
 altura = input('Qual é sua altura: ')
 altura = float(altura)
 sexo = input('Masculino ou Feminino: ')
