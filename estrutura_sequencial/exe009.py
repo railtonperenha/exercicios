811603949f0699920f3ab01e7d0d23b8e6c18148
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-08-13 08:39:30 -0300

    Update exe009.py

--- estrutura_sequencial/exe009.py
+++ estrutura_sequencial/exe009.py
@@ -1,3 +1,6 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+# C = 5 * ((F-32) / 9)
+
 fahrenheit = input('Graus em Fahrenheit: ')
 fahrenheit = int(fahrenheit)
 
