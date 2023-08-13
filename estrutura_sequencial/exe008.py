b7d9e0b9afbac8ab30ab2177a2359090e8555949
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-08-13 08:39:55 -0300

    Update exe008.py

--- estrutura_sequencial/exe008.py
+++ estrutura_sequencial/exe008.py
@@ -1,3 +1,5 @@
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
 valor_hora = input('Valor ganho por hora: R$ ')
 horas_trabalhadas = input('Horas trabalhadas: ')
 
