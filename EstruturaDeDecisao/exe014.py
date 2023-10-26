cd5a68b2f6f57f96df430886a014f1036401cfbb
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-10-25 22:51:15 -0300

    Update exe014.py

--- EstruturaDeDecisao/exe014.py
+++ EstruturaDeDecisao/exe014.py
@@ -5,6 +5,7 @@
 #   Entre 6.0 e 7.5         C
 #   Entre 4.0 e 6.0         D
 #   Entre 4.0 e zero        E
+# O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
 
 nota1 = input('Primeira Nota: ')
 nota2 = input('Segunda Nota: ')
@@ -16,17 +17,26 @@ try:
     
     if media < 4:
         conceito = 'E'
+        mensagem = 'REPROVADO!'
     elif media < 6:
         conceito = 'D'
+        mensagem = 'REPROVADO'
     elif media < 7.5:
         conceito = 'C'
+        mensagem = 'APROVADO!'
     elif media < 9:
         conceito = 'B'
+        mensagem = 'APROVADO!'
     elif media < 10:
         conceito = 'A'
+        mensagem = 'APROVADO!'
     else:
         conceito = 'Inválido'
 except:
     print("Digite apenas números!")
+
+print(f'Primeira Nota: {nota1}')
+print(f'Segunda Nota: {nota2}')
 print(f'Média de aproveitamento: {media}'.replace('.', ','))
 print(f'Conceito: {conceito}')
+print(f'{mensagem}')
