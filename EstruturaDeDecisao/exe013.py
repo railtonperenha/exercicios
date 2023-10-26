ff2df22d124d9c4a8985705e2ff02b3b3a57a1b6
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-10-25 22:14:07 -0300

    Update exe013.py

--- EstruturaDeDecisao/exe013.py
+++ EstruturaDeDecisao/exe013.py
@@ -1,5 +1,3 @@
-# Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
-
 print('''
 [1] Domingo
 [2] Segunda-Feira
@@ -9,32 +7,32 @@ print('''
 [6] Sexta-Feira
 [7] Sábado
 ''')
-dia = input('Digite um número correspondente: ')
 
-try:
-    dia = int(dia)
-    if dia == 1:
-        dia_semana = 'Domingo'
-    
-    elif dia == 2:
-        dia_semana = 'Segunda-Feira'
+while True:
+    dia = input('Digite um número correspondente: ')
 
-    elif dia == 3:
-        dia_semana = 'Terça-Feira'
-    
-    elif dia == 4:
-        dia_semana = 'Quarta-Feira'
+    try:
+        dia = int(dia)
+        if 1 <= dia <= 7:
+            if dia == 1:
+                dia_semana = 'Domingo'
+            elif dia == 2:
+                dia_semana = 'Segunda-Feira'
+            elif dia == 3:
+                dia_semana = 'Terça-Feira'
+            elif dia == 4:
+                dia_semana = 'Quarta-Feira'
+            elif dia == 5:
+                dia_semana = 'Quinta-Feira'
+            elif dia == 6:
+                dia_semana = 'Sexta-Feira'
+            elif dia == 7:
+                dia_semana = 'Sábado'
+            print()
+            print(dia_semana)
+            break  # Sai do loop quando um número válido é inserido
+        else:
+            print('Valor fora do intervalo (1 a 7)!')
+    except ValueError:
+        print('Valor Inválido!!')
 
-    elif dia == 5:
-        dia_semana = 'Quinta-Feira'
-
-    elif dia == 6:
-        dia_semana = 'Sexta-Feira'
-
-    elif dia == 7:
-        dia_semana = 'Sábado'
-    
-    print()
-    print(dia_semana)
-except:
-    print('Valor Inválido!!')
